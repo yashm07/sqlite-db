31c94cdd6ab4d22f885518339a84e752690c9723
Yash Matharu <ymatharu7@gmail.com>
2025-04-03 16:18:49 -0400

    Add new meta command - `.print_tree`

--- include/meta.h
+++ include/meta.h
@@ -24,3 +24,11 @@ typedef enum {
  * @return MetaCommandStatus enum - success or invalid.
  */
 MetaCommandStatus process_meta_command(InputBuffer* inputBuffer, Table* table);
+
+/**
+ * @brief Prints out contents of leaf node for debugging purposes.
+ *
+ * @param void* node
+ */
+void print_leaf_node(void* node);
+
--- src/meta.c
+++ src/meta.c
@@ -1,13 +1,32 @@
 #include <string.h>
 
-#include "meta.h"
 #include "connection.h"
+#include "meta.h"
+#include "node.h"
 
 MetaCommandStatus process_meta_command(InputBuffer* inputBuffer, Table* table) {
     if (strcmp(inputBuffer->buffer, ".exit") == 0) {
         close_db_connection(table);
         exit(EXIT_SUCCESS);
-      } else {
+    } 
+    // assuming single node tree for now
+    else if (strcmp(inputBuffer->buffer, ".print_tree") == 0) {
+      void* node = get_page(table->pager, 0);
+      print_leaf_node(node);
+    }
+      else {
         return META_COMMAND_INVALID;
     }
+
+    return META_COMMAND_SUCCESS;
 }
+
+void print_leaf_node(void* node) {
+  uint32_t numCells = *get_leaf_node_num_cells(node);
+
+  for (uint32_t i = 0; i < numCells; i++) {
+    uint32_t key = *get_leaf_node_key(node, i);
+
+    printf(" - %d: %d\n", i, key);
+  }
+};
